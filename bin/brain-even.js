5762fc3b60ee9fe97b35dcc37e9f1605edfe0371
Shokhrukh <shoxrux20888@mail.ru>
2023-03-05 16:31:37 +0300

    addBrain-even

---
+++ bin/brain-even.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import isEvenOrOtherwise from '../src/gameOnEven.js';
+
+isEvenOrOtherwise();
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
-const isMethod = () => {
-  const name = readlineSync.question('May I have your name?');
-  return console.log(`${'Hello'}, ${name}!`);
-};
-
-export default isMethod;
+export default function isMethod() {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name?\n');
+  console.log(`${'Hello'}, ${name}!`);
+  return name;
+}
