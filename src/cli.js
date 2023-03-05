66f845855c37432f904ce0fd71a9b8f755bf4550
Shokhrukh <shoxrux20888@mail.ru>
2023-03-05 13:40:17 +0300

    addESLint

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const isMethod = () => {
-    const name = readlineSync.question('May I have your name?');
-    return console.log(`${'Hello'}, ${name}!`);
+const isMethod = () => {
+  const name = readlineSync.question('May I have your name?');
+  return console.log(`${'Hello'}, ${name}!`);
 };
+
+export default isMethod;
