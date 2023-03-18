24bcbc0f565b0cd914ba9e43331d407242c15bd6
Shokhrukh <shoxrux20888@mail.ru>
2023-03-18 13:58:14 +0300

    changeCodeStyle

--- src/index.js
+++ src/index.js
@@ -1,50 +1,158 @@
 import readlineSync from 'readline-sync';
 import isMethod from './cli.js';
 
-const getRandomNumbers = () => {
+/** GLOBAL VARIABLES */
+
+const winMessage = 'Congratulations';
+const questionMessage = 'Question';
+const errorMessage = 'is wrong answer ;(. Correct answer was';
+const tryMessage = `${'Let\'s try again'}`;
+const answerYes = 'yes';
+const answerNo = 'no';
+
+let result = 0;
+let firstNumber = 0;
+let secondNumber = 0;
+let randomValue = 0;
+let reminderOfDivision = -1;
+
+function getRandomNumbers() {
   const maxValue = 100;
   const minValue = 0;
   return Math.round(Math.random() * (maxValue - minValue) + minValue);
-};
+}
 
-const checkResult = (answer, correctlyAnswer, name) => {
-  const errorMessage = 'is wrong answer ;(. Correct answer was';
-  const tryMessage = `${'Let\'s try again'}, ${name}!`;
+function checkResult(answer, correctlyAnswer, name) {
   console.log(`${'Your answer'}: ${answer}`);
   if (answer === correctlyAnswer) {
     console.log('Correct!');
     return 1;
   }
   console.log(`'${answer}' ${errorMessage} '${correctlyAnswer}'`);
-  console.log(tryMessage);
+  console.log(`${tryMessage}, ${name}!`);
   return -1;
-};
+}
 
-export default function universalMethod(nameGame, condition) {
-  const name = isMethod();
-  const winMessage = 'Congratulations';
-  const questionMessage = 'Question';
-  let result = 0;
-  let correctlyAnswer = 0;
-  let answer = 0;
-  let count = 0;
+function calculator(name, answer, correctlyAnswer) {
+  const randomOperation = Math.round(Math.random() * (2 - 0) + 0);
+  firstNumber = getRandomNumbers();
+  secondNumber = getRandomNumbers();
+  switch (randomOperation) {
+    case 0:
+      correctlyAnswer = firstNumber + secondNumber;
+      result = Number(readlineSync.question(`${questionMessage}: ${firstNumber} + ${secondNumber}\n`));
+      answer = checkResult(result, correctlyAnswer, name);
+      return answer;
+    case 1:
+      correctlyAnswer = firstNumber - secondNumber;
+      result = Number(readlineSync.question(`${questionMessage}: ${firstNumber} - ${secondNumber}\n`));
+      answer = checkResult(result, correctlyAnswer, name);
+      return answer;
+    case 2:
+      correctlyAnswer = firstNumber * secondNumber;
+      result = Number(readlineSync.question(`${questionMessage}: ${firstNumber} * ${secondNumber}\n`));
+      answer = checkResult(result, correctlyAnswer, name);
+      return answer;
+    default:
+      return answer;
+  }
+}
 
-  /* Variables for parityCheck */
-  const answerYes = 'yes';
-  const answerNo = 'no';
-  let randomValue = 0;
+function parityCheck(name, answer, correctlyAnswer) {
+  randomValue = getRandomNumbers();
+  result = readlineSync.question(`${'Question'}: ${randomValue}\n`).toLowerCase();
+  correctlyAnswer = randomValue % 2 === 0 ? answerYes : answerNo;
+  answer = checkResult(result, correctlyAnswer, name);
+  return answer;
+}
 
-  /* Variables for calculator */
-  let randomOperation = 0;
-  let firstNumber = 0;
-  let secondNumber = 0;
-  let reminderOfDivision = -1;
+function nod(name, answer, correctlyAnswer) {
+  firstNumber = getRandomNumbers();
+  secondNumber = getRandomNumbers();
+  result = Number(readlineSync.question(`${'Question'}: ${firstNumber} ${secondNumber}\n`));
+  if (firstNumber === secondNumber) {
+    correctlyAnswer = firstNumber;
+    answer = checkResult(result, correctlyAnswer, name);
+    return answer;
+  }
+  if (firstNumber % secondNumber === 0) {
+    correctlyAnswer = secondNumber;
+    answer = checkResult(result, correctlyAnswer, name);
+    return answer;
+  }
+  if (secondNumber % firstNumber === 0) {
+    correctlyAnswer = firstNumber;
+    answer = checkResult(result, correctlyAnswer, name);
+    return answer;
+  }
+  if (firstNumber > secondNumber) {
+    while (reminderOfDivision !== 0) {
+      reminderOfDivision = firstNumber % secondNumber;
+      if (firstNumber % reminderOfDivision === 0 && secondNumber % reminderOfDivision === 0) {
+        correctlyAnswer = reminderOfDivision;
+        break;
+      }
+      firstNumber = secondNumber;
+      secondNumber = reminderOfDivision;
+    }
+  } else {
+    while (reminderOfDivision !== 0) {
+      reminderOfDivision = secondNumber % firstNumber;
+      if (firstNumber % reminderOfDivision === 0 && secondNumber % reminderOfDivision === 0) {
+        correctlyAnswer = reminderOfDivision;
+        break;
+      }
+      secondNumber = firstNumber;
+      firstNumber = reminderOfDivision;
+    }
+  }
+  answer = checkResult(result, correctlyAnswer, name);
+  return answer;
+}
 
-  /* Variables for gameProgression */
+function progression(name, answer, correctlyAnswer) {
   let countStep = 0;
   const array = [];
   let startStep = getRandomNumbers();
+  countStep = Math.round(Math.random() * 10);
+  randomValue = Math.round(Math.random() * 9);
+  for (let j = 0; j < 10; j += 1) {
+    array[j] = startStep;
+    startStep += countStep;
+  }
+  correctlyAnswer = array[randomValue];
+  array[randomValue] = '..';
+  result = Number(readlineSync.question(`${'Question'}: ${array}\n`));
+  answer = checkResult(result, correctlyAnswer, name);
+  return answer;
+}
 
+function prime(name, answer, correctlyAnswer) {
+  let count = 0;
+
+  randomValue = getRandomNumbers();
+  result = readlineSync.question(`${'Question'}: ${randomValue}\n`).toLowerCase();
+  count = 0;
+  if (randomValue % 1 === 0 && randomValue % randomValue === 0) {
+    count += 2;
+  }
+  for (let k = 2; k <= randomValue; k += 1) {
+    if (count === 2 && k === randomValue) {
+      correctlyAnswer = answerYes;
+      break;
+    } else if (randomValue % k === 0) {
+      count += 1;
+      correctlyAnswer = answerNo;
+    }
+  }
+  answer = checkResult(result, correctlyAnswer, name);
+  return answer;
+}
+
+export default function universalMethod(nameGame, condition) {
+  let answer = 0;
+  const correctlyAnswer = 0;
+  const name = isMethod();
   console.log(condition);
   for (let i = 0; i < 3; i += 1) {
     if (answer === -1) {
@@ -52,104 +160,19 @@ export default function universalMethod(nameGame, condition) {
     }
     switch (nameGame) {
       case 'calculator':
-        randomOperation = Math.round(Math.random() * (2 - 0) + 0);
-        firstNumber = getRandomNumbers();
-        secondNumber = getRandomNumbers();
-        switch (randomOperation) {
-          case 0:
-            correctlyAnswer = firstNumber + secondNumber;
-            result = Number(readlineSync.question(`${questionMessage}: ${firstNumber} + ${secondNumber}\n`));
-            answer = checkResult(result, correctlyAnswer, name);
-            break;
-          case 1:
-            correctlyAnswer = firstNumber - secondNumber;
-            result = Number(readlineSync.question(`${questionMessage}: ${firstNumber} - ${secondNumber}\n`));
-            answer = checkResult(result, correctlyAnswer, name);
-            break;
-          case 2:
-            correctlyAnswer = firstNumber * secondNumber;
-            result = Number(readlineSync.question(`${questionMessage}: ${firstNumber} * ${secondNumber}\n`));
-            answer = checkResult(result, correctlyAnswer, name);
-            break;
-          default:
-            break;
-        }
+        answer = calculator(name, answer, correctlyAnswer);
         break;
       case 'parityCheck':
-        randomValue = getRandomNumbers();
-        result = readlineSync.question(`${'Question'}: ${randomValue}\n`).toLowerCase();
-        correctlyAnswer = randomValue % 2 === 0 ? answerYes : answerNo;
-        answer = checkResult(result, correctlyAnswer, name);
+        answer = parityCheck(name, answer, correctlyAnswer);
         break;
       case 'NOD':
-        firstNumber = getRandomNumbers();
-        secondNumber = getRandomNumbers();
-        result = Number(readlineSync.question(`${'Question'}: ${firstNumber} ${secondNumber}\n`));
-        if (firstNumber === secondNumber) {
-          correctlyAnswer = firstNumber;
-          answer = checkResult(result, correctlyAnswer, name);
-          break;
-        } else if (firstNumber % secondNumber === 0) {
-          correctlyAnswer = secondNumber;
-          answer = checkResult(result, correctlyAnswer, name);
-          break;
-        } else if (secondNumber % firstNumber === 0) {
-          correctlyAnswer = firstNumber;
-          answer = checkResult(result, correctlyAnswer, name);
-          break;
-        }
-        if (firstNumber > secondNumber) {
-          while (reminderOfDivision !== 0) {
-            reminderOfDivision = firstNumber % secondNumber;
-            if (firstNumber % reminderOfDivision === 0 && secondNumber % reminderOfDivision === 0) {
-              correctlyAnswer = reminderOfDivision;
-              break;
-            }
-            firstNumber = secondNumber;
-            secondNumber = reminderOfDivision;
-          }
-        } else {
-          while (reminderOfDivision !== 0) {
-            reminderOfDivision = secondNumber % firstNumber;
-            if (firstNumber % reminderOfDivision === 0 && secondNumber % reminderOfDivision === 0) {
-              correctlyAnswer = reminderOfDivision;
-              break;
-            }
-            secondNumber = firstNumber;
-            firstNumber = reminderOfDivision;
-          }
-        }
-        answer = checkResult(result, correctlyAnswer, name);
+        answer = nod(name, answer, correctlyAnswer);
         break;
       case 'progression':
-        countStep = Math.round(Math.random() * 10);
-        randomValue = Math.round(Math.random() * 9);
-        for (let j = 0; j < 10; j += 1) {
-          array[j] = startStep;
-          startStep += countStep;
-        }
-        correctlyAnswer = array[randomValue];
-        array[randomValue] = '..';
-        result = Number(readlineSync.question(`${'Question'}: ${array}\n`));
-        answer = checkResult(result, correctlyAnswer, name);
+        answer = progression(name, answer, correctlyAnswer);
         break;
       case 'prime':
-        randomValue = getRandomNumbers();
-        result = readlineSync.question(`${'Question'}: ${randomValue}\n`).toLowerCase();
-        count = 0;
-        if (randomValue % 1 === 0 && randomValue % randomValue === 0) {
-          count += 2;
-        }
-        for (let k = 2; k <= randomValue; k += 1) {
-          if (count === 2 && k === randomValue) {
-            correctlyAnswer = answerYes;
-            break;
-          } else if (randomValue % k === 0) {
-            count += 1;
-            correctlyAnswer = answerNo;
-          }
-        }
-        answer = checkResult(result, correctlyAnswer, name);
+        answer = prime(name, answer, correctlyAnswer);
         break;
       default:
         break;
